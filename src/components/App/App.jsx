d96aa16b82912bfea55e985ee78e5080410bcff2
OlgaDonchenko <n0931606045@gmail.com>
2024-04-03 14:44:15 +0300

    added reset

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,21 +4,37 @@ import "./App.css";
 import Description from "../Description/Description";
 import Options from "../Options/Options";
 import Feedback from "../Feedback/Feedback";
+import Notification from "../Notification/Notification ";
 
 export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [reset, setShowReset] = useState(false);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setShowReset(true);
+    // setShowNotification(false);
   };
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setShowReset(true);
+    // setShowNotification(false);
   };
   const handleBadClick = () => {
     setBad(bad + 1);
+    setShowReset(true);
+    // setShowNotification(false);
   };
+  const handleResetClick = () => {
+    setGood(0);
+    setNeutral(0);
+    setBad(0);
+    setShowReset(false);
+    // setShowNotification(true);
+  };
+
   return (
     <div>
       <Description />
@@ -27,6 +43,8 @@ export default function App() {
         onNeutralClick={handleNeutralClick}
         onBadClick={handleBadClick}
       />
+
+      {reset && <button onClick={handleResetClick}>Reset</button>}
       <Feedback good={good} neutral={neutral} bad={bad} />
     </div>
   );
